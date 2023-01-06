a358821f5ecb015c57e130756e3224c23d2e330f
Mike Fomin <mikefomin@Mikes-MacBook-Pro.local>
2023-01-05 17:54:37 -0800

    moreserver side work

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -5,7 +5,11 @@ router.post('/', async (req, res) => {
     try {
         const newUser = await User.create(req.body)
 
-        res.status(200).json(userData)
+        req.session.save(() => {
+            req.session.user_id = userData.id;
+            req.session.logged_in = true;
+            res.status(200).json(userData)
+        })
 
 
 
@@ -13,3 +17,5 @@ router.post('/', async (req, res) => {
         res.status(500).json(error)
     }
 })
+
+module.exports = router
